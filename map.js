c43197991fdd91c14aff6ffd699bb57a90a134a3
Shaily-28 <snievesadame@ucsd.edu>
2025-11-14 20:36:24 -0800

    Adding Boston lines to my map

--- map.js
+++ map.js
@@ -1,15 +1,14 @@
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoic2hhaWx5LTI4IiwiYSI6ImNtaHpyaWN5NzBwbm0ya29wcGt1bGV6ZjQifQ.tPamgMp22xEispqmV_JRSQ';
 
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2hhaWx5LTI4IiwiYSI6ImNtaHpyaWN5NzBwbm0ya29wcGt1bGV6ZjQifQ.tPamgMp22xEispqmV_JRSQ'; 
 
 console.log('Mapbox GL JS Loaded:', mapboxgl);
 
-
 const map = new mapboxgl.Map({
-  container: 'map', 
-  style: 'mapbox://styles/mapbox/streets-v12', 
-  center: [-71.09415, 42.36027], 
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v12',
+  center: [-71.09415, 42.36027],
   zoom: 12,
   minZoom: 5,
   maxZoom: 18,
@@ -17,4 +16,22 @@ const map = new mapboxgl.Map({
 
 map.on('load', () => {
   console.log('Map has loaded!');
+
+  map.addSource('boston_route', {
+    type: 'geojson',
+    data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D',
+  });
+
+  map.addLayer({
+    id: 'bike-lanes',
+    type: 'line',
+    source: 'boston_route',
+    paint: {
+      'line-color': '#32D400',
+      'line-width': 5,
+      'line-opacity': 0.6,
+    },
+  });
 });
+
+
